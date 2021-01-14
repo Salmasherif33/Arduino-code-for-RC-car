330498dfa7f1a72faa1c3f8e5c350a563cf57f59
Salma sherif <40717302+Salmasherif33@users.noreply.github.com>
2021-01-15 01:35:24 +0200

    Add files via upload

---
+++ ex.1/ex.1.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ pinMode(9,OUTPUT);
+    digitalWrite(9,HIGH);
+  delay(4000);
+  digitalWrite(9,LOW);
+  delay(4000);
+}
